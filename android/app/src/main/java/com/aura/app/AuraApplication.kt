61ed5cd4da5fd3eb7e74049594fcfac1deba41bf
prajwalkumar2343 <kprajwal3673@gmail.com>
2026-06-11 16:56:03 +0530

    chore: update android:app:src:main:java:com:aura:app:AuraApplication.kt

--- android/app/src/main/java/com/aura/app/AuraApplication.kt
+++ android/app/src/main/java/com/aura/app/AuraApplication.kt
@@ -1,14 +1,22 @@
 package com.aura.app
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class AuraApplication : Application() {
     lateinit var container: AppContainer
         private set
+    private val appScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
     override fun onCreate() {
         super.onCreate()
         container = AppContainer(this)
+        appScope.launch {
+            runCatching { container.automationRuntime.restoreTriggers() }
+        }
     }
 }
 
